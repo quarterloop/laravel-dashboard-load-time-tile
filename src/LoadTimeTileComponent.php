1a3555a3cb4bd48824b5cbaae2520f39d49cff45
Gero Wallisch <gero.wallisch@me.com>
2022-06-01 23:13:22 +0200

    Initial Upload

---
+++ src/LoadTimeTileComponent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Quarterloop\LoadTimeTile;
+
+use Livewire\Component;
+use Illuminate\Support\DB;
+
+class LoadTImeTileComponent extends Component
+{
+
+    public $position;
+
+    public function mount(string $position)
+    {
+        $this->position = $position;
+    }
+
+
+    public function render()
+    {
+
+      $loadTimeStore = LoadTimeStore::make();
+
+        return view('dashboard-load-time-tile::tile', [
+            'website'         => config('dashboard.tiles.hosting.url'),
+            'times'            => $ttfbStore->getData()['data'],
+            'lastUpdateTime'  => date('H:i:s', strtotime($ttfbStore->getLastUpdateTime())),
+            'lastUpdateDate'  => date('d.m.Y', strtotime($ttfbStore->getLastUpdateDate())),
+        ]);
+    }
+}
